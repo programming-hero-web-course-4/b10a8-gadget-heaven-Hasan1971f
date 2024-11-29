df59d004e73e40f70c9e77a68906347c4fd2e4ef
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-29 19:14:18 +0600

    added a project set up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h3>Helo app</h3>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/AllProduct.jsx
@@ -0,0 +1,9 @@
+const AllProduct = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllProduct;
