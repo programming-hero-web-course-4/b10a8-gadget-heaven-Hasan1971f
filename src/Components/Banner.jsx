17ed9a8dae4f00a231297499d79b3447ed9b372d
Hasan1971f <mdhasanurrahmanhasan101@gmail.com>
2024-12-03 08:21:37 +0600

    added a Banner.jsx

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,7 +2,7 @@ const Banner = () => {
     return (
         <div>
             <div className=" flex left-24 justify-center mb-96 z-50 absolute">
-                <img className="w-5/6"  src="https://i.ibb.co.com/fQ0vR5Q/Frame-8.png" alt="" />
+                <img className="w-5/6"  src="https://i.ibb.co/fQ0vR5Q/Frame-8.png" alt="" />
             </div>
         </div>
     );
